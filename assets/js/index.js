41ab822ee83ae59d70eb08841cfbbb5bdf232b5b
dtangen <dtangen1@gmail.com>
2018-08-13 14:15:16 -0600

    fixing layout of varieties page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,58 +1,63 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  //Modals for the recipes page
+  $("#exampleModal").on("shown.bs.modal", function() {
+    $("#myInput").trigger("focus");
+  });
 
-    //Modals for the recipes page
-    $('#exampleModal').on('shown.bs.modal', function () {
-        $('#myInput').trigger('focus')
-    })
+  //Modals for the newletter
+  $("#newsletterModal").on("shown.bs.modal", function() {
+    $("#myInput").trigger("focus");
+  });
 
-    //Modals for the newletter
-    $('#newsletterModal').on('shown.bs.modal', function () {
-        $('#myInput').trigger('focus')
-    })
+  //Maps for main page
+  $("#lenexaMap").hide();
 
-    //Maps for main page
-    $('#lenexaMap').hide();
+  $("#opMap").on("click", function() {
+    $("#overlandParkMap").show();
+    $("#lenexaMap").hide();
+  });
 
-    $('#opMap').on('click', function () {
-        $('#overlandParkMap').show();
-        $('#lenexaMap').hide();
-    })
+  $("#lenMap").on("click", function() {
+    $("#overlandParkMap").hide();
+    $("#lenexaMap").show();
+  });
 
-    $('#lenMap').on('click', function () {
-        $('#overlandParkMap').hide();
-        $('#lenexaMap').show();
-    })
+  //Microgreens 101 Page
+  $("#harvestWash").hide();
+  $("#useThem").hide();
 
-    //Microgreens 101 Page
-    $('#harvestWash').hide();
-    $('#useThem').hide();
+  //Show info for first slide
+  $("#firstSlide").on("click", function() {
+    $("#knowMicrogreens").show();
+    $("#harvestWash").hide();
+    $("#useThem").hide();
+  });
 
-    //Show info for first slide
-    $('#firstSlide').on('click', function () {
-        $('#knowMicrogreens').show();
-        $('#harvestWash').hide();
-        $('#useThem').hide();
-    });
+  //Show info for second slide
+  $("#secondSlide").on("click", function() {
+    $("#knowMicrogreens").hide();
+    $("#harvestWash").show();
+    $("#useThem").hide();
+  });
 
-    //Show info for second slide
-    $('#secondSlide').on('click', function () {
-        $('#knowMicrogreens').hide();
-        $('#harvestWash').show();
-        $('#useThem').hide();
-    });
+  //Show info for third slide
+  $("#thirdSlide").on("click", function() {
+    $("#knowMicrogreens").hide();
+    $("#harvestWash").hide();
+    $("#useThem").show();
+  });
 
-    //Show info for third slide
-    $('#thirdSlide').on('click', function () {
-        $('#knowMicrogreens').hide();
-        $('#harvestWash').hide();
-        $('#useThem').show();
-    });
+  //Product page jumbotron
+  $(".koenigHill").click(function() {
+    var koenigHill = $(this).attr("data-link");
+    window.open(koenigHill);
+    return false;
+  });
 
-    //Product page jumbotron
-    $(".koenigHill").click(function () {
-        var koenigHill = $(this).attr("data-link");
-        window.open(koenigHill);
-        return false;
-    });
+  $("#varieties").hide();
 
+  $("#living").click(function() {
+    $("#varieties").show();
+    $("#varieties-categories").hide();
+  });
 });
